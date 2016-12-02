02fee4fcdaaeb81f4377c0ecd7f6d2e17c3f320c
Pedro <p158336@g.unicamp.br>
2016-12-02 20:23:00 -0200

    teste1

--- SOUL.s
+++ SOUL.s
@@ -1,5 +1,5 @@
-@Constantes para os enderecos do GPT
-.set GPT_CR,     0x53FA0000          
+@Constantes para os enderecos do GPT test
+.set GPT_CR,     0x53FA0000
 .set GPT_PR,     0x53FA0004
 .set GPT_SR,     0x53FA0008
 .set GPT_IR,     0x53FA000C
@@ -8,7 +8,7 @@
 @Constantes para os enderecos do TZIC
 .set TZIC_BASE,             0x0FFFC000
 .set TZIC_INTCTRL,          0x0
-.set TZIC_INTSEC1,          0x84 
+.set TZIC_INTSEC1,          0x84
 .set TZIC_ENSET1,           0x104
 .set TZIC_PRIOMASK,         0xC
 .set TZIC_PRIORITY9,        0x424
@@ -16,16 +16,16 @@
 .org 0x0
 .section .iv,"a"
 
-_start:         
+_start:
 
 interrupt_vector:
     b RESET_HANDLER
 .org 0x18
     b IRQ_HANDLER
-    
+
 .data
 CONTADOR: .word 0               @Declara a variavel CONTADOR e inicializa ela com 0
-    
+
 .org 0x100
 .text
     ldr r2, =CONTADOR           @Zera o contador
@@ -34,26 +34,26 @@ CONTADOR: .word 0               @Declara a variavel CONTADOR e inicializa ela co
 
 RESET_HANDLER:
 
-    
+
     ldr r0, =interrupt_vector   @Set interrupt table base address on coprocessor 15.
     mcr p15, 0, r0, c12, c0, 0
-    
+
     ldr r0, =GPT_CR             @Confirar o GPT
     mov r1, #0x41               @Habilitar clock no periferico
     str r1, [r0]
-    
+
     ldr r0, =GPT_PR             @Zerar o prescaler
     mov r1, #0
     str r1, [r0]
-    
+
     ldr r0, =GPT_OCR1           @Contar ate 100 o clock
     mov r1, #100
     str r1, [r0]
-    
+
     ldr r0, =GPT_IR             @Habilitar a interrupcao
     mov r1, #1
     str r1, [r0]
-    
+
 SET_TZIC:
 
     @ Liga o controlador de interrupcoes
@@ -90,21 +90,20 @@ SET_TZIC:
 
     @instrucao msr - habilita interrupcoes
     msr  CPSR_c, #0x13       @ SUPERVISOR mode, IRQ/FIQ enabled
-   
+
 infinityloop:
     b infinityloop
-    
+
 IRQ_HANDLER:
     ldr r0, =GPT_SR             @Avisar que houve interrupcao
     mov r1, #0x1
     str r1, [r0]
-    
+
     ldr r0, =CONTADOR           @somar 1 ao contador
     ldr r1, [r0]
     add r1, r1, #1
     str r1, [r0]
-    
+
     sub pc, pc, #4              @Corrigir pc subtraindo pc
-    
+
     movs pc, lr
-    
