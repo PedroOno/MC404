9e0f2064e674826807109430b1f123685e862efa
P.Ono <p158336@g.unicamp.br>
2016-12-04 19:42:13 -0200

    working on reset

--- SOUL.s
+++ SOUL.s
@@ -1,11 +1,11 @@
-@Constantes para os enderecos do GPT test
-.set GPT_CR,     0x53FA0000                 @ GPT Control Register
-.set GPT_PR,     0x53FA0004                 @ GPT Prescaler Register
-.set GPT_SR,     0x53FA0008                 @ GPT Status Register
-.set GPT_IR,     0x53FA000C                 @ GPT Interrupt Register
-.set GPT_OCR1,   0x53FA0010                 @ GPT Output Compare Register 1
+@ constantes para os enderecos do GPT test
+.set GPT_CR,                0x53FA0000                 @ GPT Control Register
+.set GPT_PR,                0x53FA0004                 @ GPT Prescaler Register
+.set GPT_SR,                0x53FA0008                 @ GPT Status Register
+.set GPT_IR,                0x53FA000C                 @ GPT Interrupt Register
+.set GPT_OCR1,              0x53FA0010                 @ GPT Output Compare Register 1
 
-@Constantes para os enderecos do TZIC
+@ constantes para os enderecos do TZIC
 .set TZIC_BASE,             0x0FFFC000
 .set TZIC_INTCTRL,          0x0
 .set TZIC_INTSEC1,          0x84
@@ -13,9 +13,18 @@
 .set TZIC_PRIOMASK,         0xC
 .set TZIC_PRIORITY9,        0x424
 
+@ registradores GPIO
+.set GPIO_DR,               0x53F84000
+.set GPIO_GDIR,             0x53F84004
+.set GPIO_PSR,              0x53F84008
+
+@ GPIO_SET
+.set GPIO_SET_GDIR,         0xFFFC003E
+
+
 @ Constante para os alarmes
 .set MAX_ALARMS, 			0x8
-.set TIME_SZ,				5000
+.set TIME_SZ,				1000
 
 .org 0x0
 .section .iv,"a"
@@ -24,76 +33,127 @@ _start:
 
 interrupt_vector:
     b RESET_HANDLER
-.org 0x18
+    .org 0x8
+    b SYSCALL_HANDLER
+    .org 0x18
     b IRQ_HANDLER
 
 .org 0x100
 .text
 
 RESET_HANDLER:
-
-    ldr r2, =SYSTEM_TIME            @ zera o system time
-    mov r0, #0
-    str r0, [r2]
-
     ldr r0, =interrupt_vector       @ set interrupt table base address on coprocessor 15.
     mcr p15, 0, r0, c12, c0, 0
 
-    ldr r0, =GPT_CR                 @ confirar o GPT
-    mov r1, #0x41                   @ habilitar clock no periferico
-    str r1, [r0]
+    SET_GPT:
 
-    ldr r0, =GPT_PR                 @ zerar o prescaler
-    mov r1, #0
-    str r1, [r0]
+        ldr r2, =SYSTEM_TIME            @ zera o system time
+        mov r0, #0
+        str r0, [r2]
 
-    ldr r0, =GPT_OCR1               @ contar ate 100 o clock
-    mov r1, #100
-    str r1, [r0]
 
-    ldr r0, =GPT_IR                 @ habilitar a interrupcao
-    mov r1, #1
-    str r1, [r0]
+        ldr r0, =GPT_CR                 @ confirar o GPT
+        mov r1, #0x41                   @ habilitar clock no periferico
+        str r1, [r0]
 
-SET_TZIC:
+        ldr r0, =GPT_PR                 @ zerar o prescaler
+        mov r1, #0
+        str r1, [r0]
 
-    @ Liga o controlador de interrupcoes
-    @ R1 <= TZIC_BASE
+        ldr r0, =GPT_OCR1               @ contar ate 100 o clock
+        mov r1, #TIME_SZ
+        str r1, [r0]
 
-    ldr r1, =TZIC_BASE
+        ldr r0, =GPT_IR                 @ habilitar a interrupcao
+        mov r1, #1
+        str r1, [r0]
 
-    @ Configura interrupcao 39 do GPT como nao segura
-    mov r0, #(1 << 7)
-    str r0, [r1, #TZIC_INTSEC1]
+    SET_TZIC:
 
-    @ Habilita interrupcao 39 (GPT)
-    @ reg1 bit 7 (gpt)
+        @ Liga o controlador de interrupcoes
+        @ R1 <= TZIC_BASE
 
-    mov r0, #(1 << 7)
-    str r0, [r1, #TZIC_ENSET1]
+        ldr r1, =TZIC_BASE
 
-    @ Configure interrupt39 priority as 1
-    @ reg9, byte 3
+        @ Configura interrupcao 39 do GPT como nao segura
+        mov r0, #(1 << 7)
+        str r0, [r1, #TZIC_INTSEC1]
 
-    ldr r0, [r1, #TZIC_PRIORITY9]
-    bic r0, r0, #0xFF000000
-    mov r2, #1
-    orr r0, r0, r2, lsl #24
-    str r0, [r1, #TZIC_PRIORITY9]
+        @ Habilita interrupcao 39 (GPT)
+        @ reg1 bit 7 (gpt)
 
-    @ Configure PRIOMASK as 0
-    eor r0, r0, r0
-    str r0, [r1, #TZIC_PRIOMASK]
+        mov r0, #(1 << 7)
+        str r0, [r1, #TZIC_ENSET1]
 
-    @ Habilita o controlador de interrupcoes
-    mov r0, #1
-    str r0, [r1, #TZIC_INTCTRL]
+        @ Configure interrupt39 priority as 1
+        @ reg9, byte 3
 
-    @instrucao msr - habilita interrupcoes
-    msr  CPSR_c, #0x13          @ SUPERVISOR mode, IRQ/FIQ enabled
+        ldr r0, [r1, #TZIC_PRIORITY9]
+        bic r0, r0, #0xFF000000
+        mov r2, #1
+        orr r0, r0, r2, lsl #24
+        str r0, [r1, #TZIC_PRIORITY9]
 
-infinityloop:
-    b infinityloop
+        @ Configure PRIOMASK as 0
+        eor r0, r0, r0
+        str r0, [r1, #TZIC_PRIOMASK]
+
+        @ Habilita o controlador de interrupcoes
+        mov r0, #1
+        str r0, [r1, #TZIC_INTCTRL]
+
+        @instrucao msr - habilita interrupcoes
+        msr  CPSR_c, #0x13          @ SUPERVISOR mode, IRQ/FIQ enabled
+
+    SET_GPIO:
+        ldr r0, =GPIO_GDIR                              @ setar cada pino do GPIO como entrada(0) ou saida (1)
+        ldr r1, =GPIO_SET_GDIR                          @ salvar essa constante no registrador de direcoes
+        str r1, [r0]
+
+    SET_STACK:
+        ldr r0, =STACK_SUP_ADRESS                       @ endereco da pilha de supervisor
+        mov sp, r0                                      @ seta o stack pointer do supervisor
+
+        msr CPSR_c, #0b11111                            @ seta o modo de operacao como system
+        ldr r0, =STACK_SYS_ADRESS                       @ endereco da pilha no system
+        mov sp, r0                                      @ seta o stack pointer no system
+
+        msr CPSR_c, #0b10010                            @ seta o modo de operacao como IRQ
+        ldr r0, =STACK_IRQ_ADRESS                       @ endereco da pilha do IRQ
+        mov sp, r0                                      @ seta o stack pointer no IRQ
+
+        msr CPSR_c, #0b10000                            @ muda para modo de user
+        ldr r0, =USER_ADRESS_START
+        mov pc, r0
+
+
+SYSCALL_HANDLER:
+    stmfd sp!, {lr}                                 @ salva o link register para retorno
+
+	cmp r7, #16                                     @ read sonar
+	bleq READ_SONAR
+
+	cmp r7, #17                                     @ register proximity callback
+    bleq REGISTER_PROXIMITY_CALLBACK
+
+	cmp r7, #18                                     @ set motor speed
+	bleq SET_MOTOR_SPEED
+
+	cmp r7, #19                                     @ set motors speed
+	bleq SET_MOTORS_SPEED
+
+	cmp r7, #20                                     @ get time
+	bleq GET_TIME
+
+	cmp r7, #21                                     @ set time
+	bleq SET_TIME
+
+	cmp r7, #22                                     @ set alarm
+	bleq SET_ALARM
+
+	ldmfd sp!, {lr}                                 @ recupera o link register da pilha
+
+	movs pc, lr
 
 IRQ_HANDLER:
     ldr r0, =GPT_SR             @ avisar que houve interrupcao
@@ -108,6 +168,8 @@ IRQ_HANDLER:
     sub pc, pc, #4              @ corrigir pc subtraindo pc
 
     movs pc, lr
+
+
 
 .data
 SYSTEM_TIME: .word 0           @ SYSTEM_TIME inicializa com 0
